9335741cac755bbf063bb2d8084c911831e07324
spacecityjams <62345526+spacecityjams@users.noreply.github.com>
2020-05-19 22:15:36 -0500

    new test file and word reverse

--- application.js
+++ application.js
@@ -2,19 +2,16 @@ const fs = require('fs');
 const readline = require('readline');
 
 async function processLineByLine() {
-  const fileStream = fs.createReadStream('./dictionary/words.txt');
+  const fileStream = fs.createReadStream('./dictionary/test-words.txt');
 
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
   });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
 
   for await (const line of rl) {
-    // Each line in input.txt will be successively available here as `line`.
-    console.log(`Line from file: ${line}`);
+    
+    console.log(line.split("").reverse().join(""));
   }
 }
-
 processLineByLine();
